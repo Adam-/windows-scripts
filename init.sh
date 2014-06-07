f6946a67cb7d26a39cc229875c5f71332d985f4f
Adam <Adam@anope.org>
2014-06-07 00:49:05 -0400

    travis

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo apt-get install mingw-w64 lzip
+sudo apt-get update -qq
+sudo apt-get install -y -f gcc-mingw-w64-i686 g++-mingw-w64-i686 binutils-mingw-w64-i686 mingw-w64-dev lzip
